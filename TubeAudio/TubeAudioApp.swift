89d1b21cfb7e623c65263c26a523a107f6682f32
fukurose-jun02 <jun.fukurose@gmail.com>
2026-06-19 10:40:54 +0900

    Initial Commit

---
+++ TubeAudio/TubeAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TubeAudioApp.swift
+//  TubeAudio
+//
+//  Created by 袋瀬淳 on 2026/06/19.
+//
+
+import SwiftUI
+
+@main
+struct TubeAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
